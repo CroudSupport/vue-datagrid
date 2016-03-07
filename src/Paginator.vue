30359a2442e9641ea0e4ad0622574a139dc92fb9
Brock Reece <brockreece@gmail.com>
2016-03-07 12:42:34 +0000

    Basic paginator component

---
+++ src/Paginator.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="ui small pagination menu">
+        <a class="item" v-for="page in pageList">{{ page }}</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            active: {},
+            pages: {},
+        },
+        
+        computed: {
+            pageList() {
+                const pages = []
+                for (let i = 1; i <= this.pages; i++) {
+                    pages.push(i) 
+                }
+
+                return pages
+            },
+        },   
+    }
+</script>
